0089d75fd8ad2023b7cded362d1c98e4b46fa30d
zhanyuan.wang@ximalaya.com <dreamworks.cnn@gmail.com>
2025-09-03 17:18:44 +0800

    feat: 优化 VMSwapPage 组件，移除不必要的订单簿注释，调整布局，确保订单簿组件正常显示。

--- src/app/(web)/vm-swap/page.tsx
+++ src/app/(web)/vm-swap/page.tsx
@@ -99,12 +99,11 @@ export default function VMSwapPage() {
       </div>
 
       {/* 主要内容区域 */}
-      <div className="max-w-7xl mx-auto p-4 grid grid-cols-12 gap-6 h-[calc(100vh-120px)]">
+      <div className="max-w-7xl mx-auto p-4 grid grid-cols-12 gap-6 ">
         {/* 左侧：图表区域 */}
         <div className="col-span-8">
           <TradingChart symbol="KEKE/USDT" tokenName="KEKE" />
-          {/* 订单簿 */}
-        <OrderBook />
+          
         </div>
         {/* 右侧：交易面板和订单簿 */}
         <div className="col-span-4 space-y-6">
@@ -113,7 +112,8 @@ export default function VMSwapPage() {
         </div>
         
       </div>
-      
+      {/* 订单簿 */}
+      <OrderBook />
       {/* Toast notifications */}
       <Toaster position="top-right" />
     </div>
